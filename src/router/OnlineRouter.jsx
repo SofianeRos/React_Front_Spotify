e72927ae708e498faf1cca36573c45c4306cd666
ES_PREPA_DEV <sosoros66@icloud.com>
2026-02-17 16:49:48 +0100

    add online router

--- src/router/OnlineRouter.jsx
+++ src/router/OnlineRouter.jsx
@@ -2,6 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "../screens/ErrorScreens/ErrorPage";
 import Home from "../screens/OnlineScreens/Home";
+import Search from "../screens/OnlineScreens/Search";
+import Library from "../screens/OnlineScreens/Library";
+import Playlist from "../screens/OnlineScreens/Playlist";
+import Wishlist from "../screens/OnlineScreens/Wishlist";
+import Detail from "../screens/OnlineScreens/Detail";
 
 const OnlineRouter = createBrowserRouter([
     {
@@ -11,7 +16,29 @@ const OnlineRouter = createBrowserRouter([
             {
                 path : "/",
                 element : <Home/>
-            }
+            },
+                        {
+                path : "/search",
+                element : <Search/>
+            },
+            {
+                path : "/library",
+                element : <Library/>
+            },
+            {
+                path : "/playlist",
+                element : <Playlist/>
+            },
+            {
+                path : "/wishlist",
+                element : <Wishlist/>
+            },
+            {
+                path : "/detail/:id",
+                element : <Detail/>
+            },
+
+
         ]
     }
 ]);    
